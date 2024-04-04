112a40a872e1de321e0eec82dc56ac4820c0ea56
ajavieralejandro <amorosijavier@gmail.com>
2024-04-04 20:00:53 -0300

    avg columns...

--- dataAnalitics.py
+++ dataAnalitics.py
@@ -3,4 +3,15 @@ import pandas as pd
 import numpy as np
 
 estaciones = pd.read_csv('estaciones.csv')
-estaciones
+
+df = pd.read_csv('estaciones.csv')
+avg_column = df['temperature'].mean()
+print("Average of temperature:", avg_column)
+avg_column = df['feels_like'].mean()
+print("Average of feels like:", avg_column)
+avg_column = df['humidity'].mean()
+print("Average of humidity like:", avg_column)
+avg_column = df['wind'].mean()
+print("Average of wind like:", avg_column)
+avg_column = df['wind_gust'].mean()
+print("Average of wind gust like:", avg_column)
